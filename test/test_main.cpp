63d779b9136da8f2aeeed7a683d1432261df680b
Stephan Dollberg <bamboon@live.de>
2012-05-21 08:21:33 +0200

    updated doc & tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -7,6 +7,7 @@
 #include <bam/parallel_for_each.hpp>
 #include <bam/parallel_for.hpp>
 #include <bam/parallel_reduce.hpp>
+#include <bam/task_pool.hpp>
 #include <bam/timer.hpp>
 
 #include <vector>
@@ -81,6 +82,32 @@ TEST_CASE("parallel_reduce/3", "parallel_reduce on zero range") {
   auto joiner = [] (iter a, iter b) { return *a > *b ? a : b; };
   auto ret = bam::parallel_reduce<iter>(std::begin(v), std::end(v), std::max_element<iter>, joiner);
   CHECK(ret == std::end(v));
+}
+
+TEST_CASE("task_pool/1", "task_pool add one task") {
+  std::vector<int> v(1, 1);
+  bam::task_pool pool;
+  pool.add([&] () { v[0] *= 2; });
+  pool.wait();
+  CHECK(v[0] == 2);
+}
+
+TEST_CASE("task_pool/2", "task_pool add large amount of tasks") {
+  std::vector<int> v(10000, 1);
+  bam::task_pool pool;
+  for(auto it = std::begin(v); it != std::end(v); ++it) {
+    pool.add([] (int& x) { x *= 2; }, std::ref(*it));
+  }
+  pool.wait();
+  CHECK(std::accumulate(std::begin(v), std::end(v), 0.0) == 2 * v.size());
+}
+
+TEST_CASE("task_pool/3", "return value of function from task_pool") {
+  bam::task_pool pool;
+  auto ret1 = pool.add([] () { return 1; });
+  auto ret2 = pool.add([] () { return 2; });
+  CHECK(ret1.get() == 1);
+  CHECK(ret2.get() == 2);
 }
 
 /* notice that on some gcc versions(e.g. 4.6.3 on fedora16, ubuntu 12.04 is fine) std::this_thread::sleep_for for is bugged and won't compile.
