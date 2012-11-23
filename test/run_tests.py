4f62730decaafc330f4d7cc462e9f94eeca5c296
Stephan Dollberg <s.dollberg@tum.de>
2012-11-23 18:37:10 +0100

    updated tester

--- test/run_tests.py
+++ test/run_tests.py
@@ -4,9 +4,11 @@ import os
 import subprocess
 import argparse
 
+# creates name of executable test
 def make_name():
     return 'tester'
 
+# get all cpp files in current dir
 def get_all_files():
     files = os.listdir('.')
 
@@ -21,9 +23,11 @@ def get_all_files():
 
     return filtered_files
 
+# reads compiler option form command line args
 def make_compiler(args):
     return args.compiler 
 
+# creates standard and standard library flags
 def make_standard_flags(args):
     compiler = "".join(args.compiler)
     standard_flags = ['-std=c++11']
@@ -33,9 +37,11 @@ def make_standard_flags(args):
 
     return standard_flags
 
+# creates error flags
 def make_error_flags():
     return ['-Wall', '-Werror', '-Wextra', '-pedantic-errors']
 
+# creates compile options like -o etc
 def make_compile(files):
     if len(files) == 0:
         files = get_all_files()
@@ -43,15 +49,19 @@ def make_compile(files):
     name = make_name()
     return ['-o', name, 'test_runner.cpp'] + files
 
+# creates additional defines
 def make_defines():
     return ['-D_GLIBCXX_USE_SCHED_YIELD', '-D_GLIBCXX_USE_NANOSLEEP']
 
+# creates optimization options
 def make_optimization():
     return ['-O2']
 
+# combines all options
 def make_compile_options(args):
     return make_compiler(args) + make_standard_flags(args) + make_error_flags() + make_defines() + make_optimization() + make_compile(args.files)
 
+# actual main function, creates compile args and then compiles and runs
 def main():
     parser = argparse.ArgumentParser(description="bam test runner")
     parser.add_argument('--compiler', nargs='+', help='enter the compiler to use', default = ['clang++'], dest='compiler')
--- test/timer_test.cpp
+++ test/timer_test.cpp
@@ -1,6 +1,7 @@
 #include "../include/bam/timer.hpp"
 #include "catch.hpp"
 
+#include <thread>
 
 /* notice that on some gcc versions(e.g. 4.6.3 on fedora16, ubuntu 12.04 is fine) std::this_thread::sleep_for for is bugged/wrong configured and won't compile.
  If it doesn't work, you need to use the following flag on compiling -D_GLIBCXX_USE_NANOSLEEP or just configure your gcc to make it standard */
