7358236892d1bc7c85d89be14b31cbba7dc48946
Stephan Dollberg <s.dollberg@tum.de>
2013-03-20 13:35:23 +0100

    changed from uref to vlaue, indentation fixes

--- include/bam/parallel_for_each.hpp
+++ include/bam/parallel_for_each.hpp
@@ -14,46 +14,45 @@ namespace bam {
 
 
     template<typename range, typename worker_predicate>
-    void parallel_for_each(const range& rng, worker_predicate&& worker, int grainsize) {
-        parallel_for_each(boost::begin(rng), boost::end(rng), std::forward(worker), grainsize);
+    void parallel_for_each(const range& rng, worker_predicate worker, int grainsize) {
+        parallel_for_each(boost::begin(rng), boost::end(rng), std::move(worker), grainsize);
     }
 
-//! parallel_for algorithm, replacing serial std::for_each loops
-/**
- * \param begin begin iterator of the range to be worked on
- * \param end end iterator of the range to be worked on
- * \param worker function object predicate which the threads will run to operate on the given range
- * \param grainsize defines the grainsize, default argument of 0 means that grainsize will be determined on runtime
- */
-template<typename ra_iter, typename worker_predicate>
-void parallel_for_each(ra_iter begin, ra_iter end, worker_predicate worker, int grainsize = 0) {
+    //! parallel_for algorithm, replacing serial std::for_each loops
+    /**
+     * \param begin begin iterator of the range to be worked on
+     * \param end end iterator of the range to be worked on
+     * \param worker function object predicate which the threads will run to operate on the given range
+     * \param grainsize defines the grainsize, default argument of 0 means that grainsize will be determined on runtime
+     */
+    template<typename ra_iter, typename worker_predicate>
+    void parallel_for_each(ra_iter begin, ra_iter end, worker_predicate worker, int grainsize = 0) {
 
-  // get params work_piece_per_thread and grainsize
-  auto work_piece_per_thread = 0;
-  std::tie(grainsize, work_piece_per_thread) = detail::get_scheduler_params(end - begin, grainsize);
+        // get params work_piece_per_thread and grainsize
+        auto work_piece_per_thread = 0;
+        std::tie(grainsize, work_piece_per_thread) = detail::get_scheduler_params(end - begin, grainsize);
 
-  if(work_piece_per_thread == 0) {
-    return;
-  }
+        if(work_piece_per_thread == 0) {
+            return;
+        }
 
-  // build work
-  auto work = detail::make_work(begin, end, work_piece_per_thread, grainsize);
+        // build work
+        auto work = detail::make_work(begin, end, work_piece_per_thread, grainsize);
 
-  // helper function which the threads will run
-  auto work_helper = [&] (typename decltype(work)::iterator thread_iter) {
-    std::pair<ra_iter, ra_iter> work_chunk;
-    while(thread_iter->try_fetch_work(work_chunk, work)) {
-      std::for_each(work_chunk.first, work_chunk.second, worker);
+        // helper function which the threads will run
+        auto work_helper = [&] (typename decltype(work)::iterator thread_iter) {
+            std::pair<ra_iter, ra_iter> work_chunk;
+            while(thread_iter->try_fetch_work(work_chunk, work)) {
+                std::for_each(work_chunk.first, work_chunk.second, worker);
+            }
+        };
+
+        // spawn tasks
+        auto tasks = detail::spawn_tasks(std::begin(work), std::end(work), work_helper);
+
+        // get tasks & rethrow exceptions
+        detail::get_tasks(std::begin(tasks), std::end(tasks));
     }
-  };
-
-  // spawn tasks
-  auto tasks = detail::spawn_tasks(std::begin(work), std::end(work), work_helper);
-
-  // get tasks & rethrow exceptions
-  detail::get_tasks(std::begin(tasks), std::end(tasks));
-}
-
 }
 
 
