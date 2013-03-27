4dbd718f9b3cd0c2f1079777533eda1d977e2137
Stephan Dollberg <s.dollberg@tum.de>
2013-03-27 22:28:43 +0100

    changed parallel_for_each implementation to parallel_for

--- include/bam/parallel_for_each.hpp
+++ include/bam/parallel_for_each.hpp
@@ -6,6 +6,8 @@
 #define BAM_PARALLEL_FOR_EACH_HPP
 
 #include "detail/parallel_utility.hpp"
+#include "parallel_for.hpp"
+
 #include <boost/range.hpp>
 #include <iterator>
 #include <algorithm>
@@ -16,36 +18,6 @@
 
 namespace bam {
 
-
-    template<typename ra_iter, typename worker_predicate>
-    void parallel_for_each_impl(ra_iter begin, ra_iter end, worker_predicate worker, int grainsize) {
-
-        // get params work_piece_per_thread and grainsize
-        auto work_piece_per_thread = 0;
-        std::tie(grainsize, work_piece_per_thread) = detail::get_scheduler_params(end - begin, grainsize);
-
-        if(work_piece_per_thread == 0) {
-            return;
-        }
-
-        // build work
-        auto work = detail::make_work(begin, end, work_piece_per_thread, grainsize);
-
-        // helper function which the threads will run
-        auto work_helper = [&] (typename decltype(work)::iterator thread_iter) {
-            std::pair<ra_iter, ra_iter> work_chunk;
-            while(thread_iter->try_fetch_work(work_chunk, work)) {
-                std::for_each(work_chunk.first, work_chunk.second, worker);
-            }
-        };
-
-        //// spawn tasks
-        auto tasks = detail::spawn_tasks(std::begin(work), std::end(work), work_helper);
-
-        //// get tasks & rethrow exceptions
-        detail::get_tasks(std::begin(tasks), std::end(tasks));
-    }
-
     /**
      * \brief parallel_for algorithm, replacing serial std::for_each loops
      * \param begin begin iterator of the range to be worked on
@@ -55,11 +27,11 @@ namespace bam {
      */
     template<typename ra_iter, typename worker_predicate>
     void parallel_for_each(ra_iter begin, ra_iter end, worker_predicate worker, int grainsize = 0) {
-#ifdef BAM_USE_TBB
-        tbb::parallel_for_each(begin, end, std::move(worker));
-#else
-        parallel_for_each_impl(begin, end, std::move(worker), grainsize);
-#endif
+        auto worker_helper = [=] (ra_iter b, ra_iter e) {
+            std::for_each(b, e, worker);
+        };
+
+        parallel_for(begin, end, worker_helper, grainsize);
     }
 
     /**
